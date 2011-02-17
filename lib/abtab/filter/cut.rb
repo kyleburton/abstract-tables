ccc5b425e10ba45cf0eefb1e475605d14463e8cf
Kyle Burton <kburton@relaynetwork.com>
2011-02-16 23:24:41 -0500

    updated cut to wrap another driver, ranges not yet working

--- lib/abtab/filter/cut.rb
+++ lib/abtab/filter/cut.rb
@@ -2,40 +2,37 @@ require 'abtab/filter'
 
 module Abtab
   class Filter::Cut < Abtab::Filter
-    def initialize url
-      @url = url
-    end
-
     def set_cut_column_spec spec
       specs = spec.split ','
-      @field_sets = []
+      @field_specs = []
       specs.each do |fset|
         if fset =~ /^(\d+)-(\d+)$/
-          @field_sets << {:type => :range,
+          @field_specs << {:type => :range,
                           :from => $1,
                           :to   => $2}
         elsif fset =~ /^(\d+)-$/
-          @field_sets << {:type => :open_from,
+          @field_specs << {:type => :open_from,
                           :from => $1}
         elsif fset =~ /^-(\d+)$/
-          @field_sets << {:type => :open_to,
+          @field_specs << {:type => :open_to,
                           :to   => $1}
         else
-          @field_sets << {:type => :single,
+          @field_specs << {:type => :single,
                           :field => fset}
         end
       end
+      puts "Set field specs: #{@field_specs.inspect}"
     end
 
     def make_slice_fn
-      if ! @columns
+      if @field_specs.nil? || @field_specs.empty?
         raise "Error: you must set the columns before peforming a cut operation"
       end
 
       @fields = []
       @field_specs.each do |spec|
         if :range == spec[:type]
-          Range.new($1.to_i, $2.to_i).each do |ii|
+          Range.new($1.to_i - 1, $2.to_i - 1).each do |ii|
             @fields << ii
           end
         elsif :open_from == spec[:type]
@@ -43,7 +40,7 @@ module Abtab
         elsif :open_to == spec[:type]
           raise "Not supported at this time: open ranges"
         elsif :single == spec[:type]
-          @fields << spec[:field].to_i
+          @fields << spec[:field].to_i - 1
         end
       end
 
@@ -60,32 +57,14 @@ module Abtab
       @slice_fn ||= make_slice_fn
     end
 
-    def open_for_reading url=@url
-      @url = url
-      @driver = Abtab.read_handle @url
-    end
-
     def next_record
-      rec = nil
+      rec = @driver.next_record
+      return nil if rec.nil?
       slice_fn.call(rec)
     end
 
-    def set_columns cols
-      @driver.set_columns cols
-      @columns = cols
-    end
-
     def columns
-      slice_fn.call(@columns)
-    end
-
-    def open_for_writing url=@url
-      raise "Error: grep does not support writing."
-      @driver = Abtab.write_handle @url
-    end
-
-    def write_record
-      raise "Error: grep does not support writing."
+      slice_fn.call(@driver.columns)
     end
 
   end
